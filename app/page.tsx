79caac83d61e2f2950338dd66ab686edd92b3505
OFNEILL <oneillj06@outlook.com>
2024-06-07 18:15:20 +0100

    Complete output and lines

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 
 import { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
-import Bezier from "../components/Bezier";
-import Line from "@/components/Line";
-import LineTo, { SteppedLineTo } from "react-lineto";
 import Currency from "@/components/Currency";
 
 export default function Home() {
   const fromRef = useRef(null);
   const [fromValue, setFromValue] = useState(0);
+  const [chosen, setChosen] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="lex flex-col items-start w-full">
@@ -28,22 +26,39 @@ export default function Home() {
             console.log(fromValue);
           }}
         >
-          <div className="A">
-            <input
-              className="text-white bg-[#38393c] border-2 border-[#38393c] rounded-lg text-center p-2 focus:appearance-none focus:m-0 outline-none focus:border-[#818181]"
-              type="number"
-              onChange={(e) => {
-                setFromValue(parseInt(e.target.value));
-              }}
-              placeholder="Enter starting amount"
-              id="fromValue"
+          <input
+            className="fromValue text-white bg-[#38393c] border-2 border-[#38393c] rounded-lg text-center p-2 focus:appearance-none focus:m-0 outline-none focus:border-[#818181]"
+            type="number"
+            onChange={(e) => {
+              setFromValue(parseInt(e.target.value));
+            }}
+            placeholder="Enter starting amount"
+            id="fromValue"
+          />
+          <div className="flex flex-col gap-4">
+            <Currency
+              currency={"USD"}
+              amount={fromValue}
+              chosen={chosen}
+              setChosen={setChosen}
+            />
+            <Currency
+              currency={"EUR"}
+              amount={fromValue}
+              chosen={chosen}
+              setChosen={setChosen}
             />
           </div>
-          <div className="ORIGIN"></div>
-          <div className="flex flex-col gap-4">
-            <Currency currency={"USD"} amount={fromValue} />
-            <Currency currency={"EUR"} amount={fromValue} />
-          </div>
+          <input
+            className="toValue cursor-copy bg-[#38393c] border-2 border-[#38393c] rounded-lg text-center p-2 focus:appearance-none focus:m-0 outline-none focus:border-[#818181]"
+            type="number"
+            disabled={true}
+            onChange={(e) => {
+              setFromValue(parseInt(e.target.value));
+            }}
+            placeholder="...."
+            id="fromValue"
+          />
         </form>
       </div>
     </main>
--- components/Currency.tsx
+++ components/Currency.tsx
@@ -4,29 +4,39 @@ import { SteppedLineTo } from "react-lineto";
 type CurrencyProps = {
   currency: string;
   amount: number;
-  otherClicked: boolean;
+  chosen: string;
+  setChosen: any;
 };
 
 export default function Currency(props: CurrencyProps) {
   //methond that hits BE, passing currency and amount
-  const [clicked, setCLicked] = useState(false);
   return (
     <div>
       <span
         className={`${props.currency} select-none cursor-pointer text-[#818181] plasmo-text-xl flex justify-center px-4 py-4 bg-[#38393c] rounded-lg`}
-        onClick={() => setCLicked(!clicked)}
+        onClick={() => props.setChosen(props.currency)}
       >
         {props.currency}
       </span>
 
       <SteppedLineTo
-        borderColor={`${clicked ? "white" : "#818181"} `}
-        from="A"
+        borderColor={`${props.currency === props.chosen ? "white" : "#818181"} `}
+        from="fromValue"
         fromAnchor="right"
         to={props.currency}
         toAnchor="left"
         delay={true}
-        zIndex={clicked ? 100 : 1}
+        zIndex={props.chosen === props.currency ? 100 : 1}
+        orientation="h"
+      />
+      <SteppedLineTo
+        borderColor={`${props.currency === props.chosen ? "white" : "#818181"} `}
+        from={props.currency}
+        fromAnchor="right"
+        to="toValue"
+        toAnchor="left"
+        delay={true}
+        zIndex={props.chosen === props.currency ? 100 : 1}
         orientation="h"
       />
     </div>
